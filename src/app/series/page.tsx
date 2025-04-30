588a324533621ef00bfaa75e383492b1faa77e8b
sh4dow18 <ramses.solano.arias@gmail.com>
2025-04-30 16:18:26 -0600

    config: main series page
    The following was done:
    - Remove Search Input Bar
    - Add Search Link Button

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -3,10 +3,12 @@ import { Slider, UpdateContent } from "@/components";
 import { FindAnimes, FindSeriesByProp } from "@/lib/series";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 import { Metadata } from "next";
+import Link from "next/link";
 // Series Page Metadata
 export const metadata: Metadata = {
   title: "Series",
-  description: "Aquí se pueden ver todas las series que ofrece Miteve para reproducir",
+  description:
+    "Aquí se pueden ver todas las series que ofrece Miteve para reproducir",
 };
 // Series Page Main Function
 function SeriesPage() {
@@ -26,29 +28,26 @@ function SeriesPage() {
     // Series Page Main Container
     <div className="flex flex-col gap-6 px-7 py-7 lg:px-10">
       {/* Series Page Utilities Container */}
-      <div className="flex flex-col gap-3 min-[500px]:items-center min-[500px]:flex-row min-[500px]:gap-5 min-[500px]:place-content-between">
+      <div className="flex place-content-between gap-3 min-[500px]:items-center">
         {/* Series Page Utilities Title */}
         <h1 className="text-[2.5rem] leading-none font-bold text-gray-300 min-[351px]:text-5xl">
           Series
         </h1>
-        {/* Series Page Utilities CTA Container */}
+        {/* Movies Page Utilities CTA Container */}
         <div className="flex items-center gap-5">
-          {/* Series Page Utilities CTA Update Content Button */}
+          {/* Movies Page Utilities CTA Update Content Button */}
           <UpdateContent type="series" />
-          {/* Series Page Utilities CTA Search */}
-          <div className="flex place-content-between gap-2 rounded-md outline-2 py-2 px-1 bg-gray-800 outline-gray-800 focus-within:outline-mateoryPurple min-[576px]:my-3">
-            <MagnifyingGlassIcon className="w-7 h-7" />
-            <input
-              type="search"
-              name="search"
-              disabled
-              className="w-full bg-transparent outline-hidden text-white"
-            />
-          </div>
+          {/* Movies Page Utilities CTA Search Button */}
+          <Link
+            href="/search"
+            className="bg-gray-800 p-2 rounded-md cursor-pointer hover:bg-gray-700"
+          >
+            <MagnifyingGlassIcon className="w-5 h-5 md:w-7 md:h-7" />
+          </Link>
         </div>
       </div>
       {/* Series Page Sliders Container */}
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5 md:gap-7">
         {SLIDERS_LIST.map((slider, index) => (
           <Slider
             key={index}
