88352dd522ed4cd14b80d60d639460005b35a975
sh4dow18 <ramses.solano.arias@gmail.com>
2025-06-19 23:17:43 -0600

    fix: middleware
    Fix health fetch in middleware to set a timeout

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,13 +14,18 @@ export async function middleware(request: NextRequest) {
     PAGE_NAME === "/error"
   ) {
     let apiOk = true;
+    const CONTROLLER = new AbortController();
+    const TIMEOUT = setTimeout(() => CONTROLLER.abort(), 300);
     try {
       const RESPONSE = await fetch(`${API_HOST_IP}/api/utils/health`, {
         method: "HEAD",
+        signal: CONTROLLER.signal,
       });
       apiOk = RESPONSE.ok;
     } catch {
       apiOk = false;
+    } finally {
+      clearTimeout(TIMEOUT);
     }
     // If api is up and the current page is "Error", redirect to movies page
     if (apiOk === true && PAGE_NAME === "/error") {
@@ -31,7 +36,10 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/error", request.url));
     }
     // If it is the admin page, check if the secret password was sent, if not, redirect to 404
-    if (PAGE_NAME === "/admin" && request.nextUrl.searchParams.get("secret") !== SECURITY_PASSWORD) {
+    if (
+      PAGE_NAME === "/admin" &&
+      request.nextUrl.searchParams.get("secret") !== SECURITY_PASSWORD
+    ) {
       return NextResponse.redirect(new URL("/404", request.url));
     }
   }
