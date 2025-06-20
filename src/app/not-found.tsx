8a1cbc851a415b03bbf2730473d409f827c0e4ca
sh4dow18 <ramses.solano.arias@gmail.com>
2025-06-19 22:59:20 -0600

    fix: not found page
    Fix not found page adding page metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,11 @@
 // Not Found Page Requirements
 import { NotFound } from "@/components";
+import { Metadata } from "next";
+// Not Found Page Metadata
+export const metadata: Metadata = {
+  title: "Página no Encontrada",
+  description: "Lo sentimos, no se pudo encontrar el contenido que está buscando",
+};
 // Not Found Page Main Function
 function NotFoundPage() {
   // Returns Not Found Page
