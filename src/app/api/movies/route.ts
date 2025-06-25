df335adc91e82b968beb58c000b0e8d4ca1c4d1a
sh4dow18 <ramses.solano.arias@gmail.com>
2025-06-25 02:43:09 -0600

    add: update container
    The following was added:
    - Update Container Movie Endpoint
    - Update Container Series Endpoint
    - Update Container Movie Form
    - Update Container Series Form

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -18,3 +18,20 @@ export async function POST(request: Request) {
     status: RESPONSE.status,
   });
 }
+// Update Movie Endpoint Main Function
+export async function PUT(request: Request) {
+  const BODY = await request.json();
+  // Update Movie to internal API endpoint
+  const RESPONSE = await fetch(`${API_HOST_IP}/api/movies`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(BODY),
+  });
+  // Return the response from the internal API as the final result
+  return new Response(JSON.stringify(await RESPONSE.json()), {
+    headers: { "Content-Type": "application/json" },
+    status: RESPONSE.status,
+  });
+}
