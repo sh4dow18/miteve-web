cd68a453eefa9ec4595c5f3e678c3c8ba5a5ad8f
sh4dow18 <ramses.solano.arias@gmail.com>
2026-01-06 23:58:43 -0600

    update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,18 +2,26 @@
 import type { MetadataRoute } from "next";
 // Manifest Main Function
 export default function manifest(): MetadataRoute.Manifest {
-  // Returns Manifest JSON File
   return {
     name: "Miteve",
     short_name: "Miteve",
-    description: `La forma más sencilla de tener tu propio "Netflix en casa"`,
+    description: 'La forma más sencilla de tener tu propio "Netflix en casa"',
     start_url: "/",
     display: "standalone",
+    orientation: "any",
+    background_color: "#000000",
+    theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
